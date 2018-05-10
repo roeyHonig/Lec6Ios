b273a234d8296d316886216703d726ecc516f9c5
HackerU <hackeru@HackerUs-iMac.local>
2018-05-09 23:17:28 -0700

    Initial Commit

---
+++ Lec6Ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lec6Ios
+//
+//  Created by HackerU on 09/05/2018.
+//  Copyright © 2018 HackerU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
